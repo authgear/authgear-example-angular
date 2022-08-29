b8124d5afdfcf3de70f0110e2710c121f4833f99
andychow326 <andychchow326@gmail.com>
2022-08-29 14:47:21 +0800

    Generate home component by Angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthRedirectComponent } from './auth-redirect/auth-redirect.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthRedirectComponent
+    AuthRedirectComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
