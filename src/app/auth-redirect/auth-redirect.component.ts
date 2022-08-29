14e9717ae6e4d167604ed760e4545891ac2beb0a
andychow326 <andychchow326@gmail.com>
2022-08-29 14:36:46 +0800

    Generate auth-redirect component by Angular CLI

---
+++ src/app/auth-redirect/auth-redirect.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-redirect',
+  templateUrl: './auth-redirect.component.html',
+  styleUrls: ['./auth-redirect.component.css']
+})
+export class AuthRedirectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
