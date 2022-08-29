a0c63b46f7bb9fbfff687226aacab432c1e70c25
andychow326 <andychchow326@gmail.com>
2022-08-29 15:30:02 +0800

    Add login button to Home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import authgear from '@authgear/web';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(public user: UserService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  startLogin(): void {
+    authgear
+      .startAuthentication({
+        redirectURI: 'http://localhost:4000/auth-redirect',
+        prompt: 'login',
+      })
+      .then(
+        () => {
+          // started authorization, user should be redirected to Authgear
+        },
+        (err) => {
+          // failed to start authorization
+          console.error(err);
+        }
+      );
   }
-
 }
