f5c65ce3197917d76b0fa929babb754db9ecc554
andychow326 <andychchow326@gmail.com>
2022-08-29 14:36:15 +0800

    Add init authgear function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import authgear from '@authgear/web';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'authgear-example-angular';
+export class AppComponent implements OnInit {
+  // configure Authgear container instance
+  initAuthgear(): Promise<void> {
+    return authgear.configure({
+      endpoint: '<your_app_endpoint>',
+      clientID: '<your_client_id>',
+      sessionType: 'refresh_token',
+    });
+  }
+
+  ngOnInit(): void {
+    this.initAuthgear().catch((e) => {
+      // Error handling
+      console.log(e);
+    });
+  }
 }
