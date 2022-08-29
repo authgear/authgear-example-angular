47d0e940a03db0988c487d23c4b2d0e2aa5a4d43
andychow326 <andychchow326@gmail.com>
2022-08-29 14:47:46 +0800

    Implment AuthRedirectComponent

--- src/app/auth-redirect/auth-redirect.component.ts
+++ src/app/auth-redirect/auth-redirect.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import authgear from '@authgear/web';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth-redirect',
   templateUrl: './auth-redirect.component.html',
-  styleUrls: ['./auth-redirect.component.css']
+  styleUrls: ['./auth-redirect.component.css'],
 })
 export class AuthRedirectComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router, private user: UserService) {}
 
   ngOnInit(): void {
+    authgear
+      .finishAuthentication()
+      .catch((e) => console.error(e))
+      .then(() => {
+        this.router.navigate(['']);
+      });
   }
-
 }
